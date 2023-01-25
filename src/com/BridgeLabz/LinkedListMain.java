0a899c3a45505c69ae5b8210cfd8da608454aa6e
nisha <nishantsingh187@gmail.com>
2023-01-25 15:34:46 +0530

    Linked list

---
+++ src/com/BridgeLabz/LinkedListMain.java
@@ -0,0 +1,31 @@
+package com.BridgeLabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.push(70);
+        linkedList.push(56);
+
+        linkedList.insert(30);
+
+
+        linkedList.display();
+        System.out.println();
+
+        if (linkedList.search(30) != null) {
+            System.out.println("Data found");
+        } else {
+            System.out.println("Data not found");
+        }
+        System.out.println(linkedList.insertAfter(30, 40));
+        System.out.println();
+        linkedList.display();
+        System.out.println();
+
+        linkedList.search(40);
+        linkedList.display();
+        System.out.println();
+        System.out.println(linkedList.size());
+
+    }
+}
